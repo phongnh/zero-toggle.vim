080309545f6b9b53f12058eb26959dc484f25665
Phong Nguyen <nhphong1406@gmail.com>
2026-04-10 17:13:03 +0700

    Remove module init

--- plugin/zero_toggle.lua
+++
@@ -1,9 +0,0 @@
--- plugin/zero_toggle.lua - Toggle common Vim settings (Neovim Lua)
--- Maintainer:   Phong Nguyen
-
-if vim.g.loaded_zero_toggle then
-  return
-end
-vim.g.loaded_zero_toggle = 1
-
-require("zero_toggle").setup()
